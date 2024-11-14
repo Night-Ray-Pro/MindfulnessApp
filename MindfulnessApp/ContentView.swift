e6b6943140ebc8a8aad4d996ae3ae37ca2e01bb3
Night Ray <oskarkapusniak@gmail.com>
2024-11-14 19:13:30 +0100

    Initial Commit

---
+++ MindfulnessApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindfulnessApp
+//
+//  Created by Oskar Kapuśniak on 14/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindfulnessApp/MindfulnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulnessAppApp.swift
+//  MindfulnessApp
+//
+//  Created by Oskar Kapuśniak on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MindfulnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
