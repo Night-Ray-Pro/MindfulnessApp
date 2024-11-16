0826d77cb7c3197d0b3d95397537a6166f7c4095
Night Ray <oskarkapusniak@gmail.com>
2024-11-16 22:39:18 +0100

    Meditation Screen Graphic

--- MindfulnessApp/MeditationView.swift
+++ MindfulnessApp/MeditationView.swift
@@ -11,9 +11,53 @@ struct MeditationView: View {
     var body: some View {
         NavigationStack{
             ZStack{
+                
+                Rectangle()
+                    .foregroundStyle(Color("MeditationBackground"))
+                    .ignoresSafeArea()
+                
+                VStack{
+                    Spacer()
+                    
+                    Image(.meditationVector2)
+                        .resizable()
+                        .scaledToFit()
+                        .ignoresSafeArea()
+                        
+                
+                }
+                
                 VStack{
                     Image(.meditationVector)
+                        .resizable()
+                        .scaledToFit()
+                        .ignoresSafeArea()
+                    
+                    Spacer()
+
                 }
+                
+                VStack{
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 22)
+                        .frame(width: 330, height: 106)
+                        .foregroundStyle(.ultraThinMaterial)
+                        .padding(7)
+                    
+                    RoundedRectangle(cornerRadius: 22)
+                        .frame(width: 330, height: 221)
+                        .foregroundStyle(.ultraThinMaterial)
+                        .padding(7)
+                    
+                    RoundedRectangle(cornerRadius: 22)
+                        .frame(width: 195, height: 58)
+                        .foregroundStyle(.ultraThinMaterial)
+                        .padding(.bottom, 35.0)
+                        .padding(.top, 7)
+                }
+                
             }
             .toolbarBackground(Color("Meditation"), for: .tabBar)
             .toolbarBackground(.visible, for: .tabBar)
