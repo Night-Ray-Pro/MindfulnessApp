44aebc051b7b16e95345709e05c3bd74adea4080
Night Ray <oskarkapusniak@gmail.com>
2024-11-17 15:59:21 +0100

    HomeViewGraphics

--- MindfulnessApp/HomeView.swift
+++ MindfulnessApp/HomeView.swift
@@ -6,17 +6,38 @@
 //
 
 import SwiftUI
+import BottomSheet
 
 struct HomeView: View {
+    @State private var hour:Int = Calendar.current.component(.hour, from: .now)
     var body: some View {
         NavigationStack{
             VStack{
-                Text("HomeDay")
+                if hour > 5 && hour < 18{
+                    LightView()
+                } else{
+                    DarkView()
+                }
+                
             }
             .toolbarBackground(Color("HomeDay"), for: .tabBar)
             .toolbarBackground(.visible, for: .tabBar)
             .toolbarColorScheme(.dark, for: .tabBar)
         }
+    }
+}
+
+struct LightView: View{
+    var body: some View {
+        Image(.homeLightVector)
+            .resizable()
+            .ignoresSafeArea()
+    }
+}
+
+struct DarkView: View{
+    var body: some View {
+        Text("Dark")
     }
 }
 
