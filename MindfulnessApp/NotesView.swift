f7cf4698d7b6f5cb8e165c2b85536d595ebf496d
Night Ray <oskarkapusniak@gmail.com>
2024-11-18 02:19:00 +0100

    ScrollToTheTop Function added for notes

--- MindfulnessApp/NotesView.swift
+++ MindfulnessApp/NotesView.swift
@@ -42,39 +42,51 @@ struct NotesView: View {
                         
                     }
                     .padding(.top, 35)
-                    .padding(.bottom, 5)
+//                    .padding(.bottom, 5)
                     .padding(.horizontal,20)
                     Text("Oskar's Journal")
                         .font(.custom("SignPainter", size: 60))
                         .foregroundStyle(.white)
+//                        .padding(.top,-5)
 
                     
                     Text("I’m creating the life of my dreams.")
                         .font(.system(size: 12, weight: .regular, design: .default))
                         .foregroundStyle(.white)
-                        .padding(.top, 2)
-                        .padding(.bottom, 10)
+                        .padding(.top, -5)
+                        .padding(.bottom, 15)
                         
                     
-                    Image(.journalVector3)
                     
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 352, height: 50)
-                        .foregroundStyle(.ultraThinMaterial)
-                    
-
-                    
-                    ScrollView{
+                    ScrollViewReader { value in
                         
-                        ForEach(1..<10){ num in
-                            RoundedRectangle(cornerRadius: 20)
-                                .frame(width: 352, height: 293)
-                                .foregroundStyle(.ultraThinMaterial)
-                                .padding(.bottom, 30)
-                            
+                        Button{
+                            withAnimation{
+                                value.scrollTo(0)
+                            }
+                        } label: {
+                            Image(.journalVector3)
                         }
+    
+                        
+                        ScrollView{
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 352, height: 50)
+                                .foregroundStyle(.ultraThinMaterial)
+                                .id(0)
+                            
+                            ForEach(1..<10){ num in
+                                RoundedRectangle(cornerRadius: 20)
+                                    .frame(width: 352, height: 293)
+                                    .foregroundStyle(.ultraThinMaterial)
+                                    .padding(.bottom, 30)
+                                    .id(num)
+                                
+                            }
+                        }
+                        .scrollIndicators(.hidden)
                     }
-                    .scrollIndicators(.hidden)
                 }
                 
                 VStack{
