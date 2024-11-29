e2b68f94c803a0bd90b0f7169dfa6df61298c3a8
Night Ray <oskarkapusniak@gmail.com>
2024-11-29 01:43:31 +0100

    Added a new functionality to maps(get user location)

--- MindfulnessApp/Journal/MapView.swift
+++ MindfulnessApp/Journal/MapView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
 
 
 struct MapView: View {
+    let locationFetcher = LocationFetcher()
+    @State private var localPosition = false
+    @State private var timesOfPositionChange = 0
     @Binding var locationName : String?
     @Binding var latitude: Double?
     @Binding var longitude: Double?
@@ -24,18 +28,61 @@ struct MapView: View {
     var body: some View {
         NavigationStack{
             ZStack{
-                Map(initialPosition: position)
+                Map(position: $position)
                     .mapStyle(.standard)
                     .onMapCameraChange { context in
 //                        print(context.region)
-                        let newPosition = MapCameraPosition.region(context.region)
-                        position = newPosition
+                        if localPosition == true && timesOfPositionChange < 1 {
+                            
+                            timesOfPositionChange += 1
+                        } else{
+                            timesOfPositionChange = 0
+                            withAnimation{
+                                localPosition = false
+                            }
+                            let newPosition = MapCameraPosition.region(context.region)
+                            position = newPosition
+                        }
                     }
                 Image(.mapPointVector)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 50, height: 100)
                     .offset(y: -40)
+                VStack{
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        Button{
+                            locationFetcher.start { location in
+                                if let location = location {
+                                    withAnimation {
+                                        localPosition = true
+                                        position = MapCameraPosition.region(
+                                            MKCoordinateRegion(
+                                                center: location,
+                                                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                            )
+                                        )
+                                    }
+                                } else {
+                                    print("Your location is unknown")
+                                }
+                            }
+                        } label: {
+                            Image(systemName: localPosition ? "location.fill" : "location")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width:25, height: 25)
+                                .foregroundStyle(.black.opacity(0.5))
+                                .padding(10)
+                                .background(.ultraThickMaterial)
+                                .preferredColorScheme(.light)
+                                .clipShape(.rect(cornerRadius: 10))
+                        }
+                    }
+                    .padding(.horizontal)
+                }
             }
             .navigationTitle("Add Location")
             .navigationBarTitleDisplayMode(.inline)
@@ -64,7 +111,33 @@ struct MapView: View {
                 
             }
         }
+        
 
+    }
+}
+
+
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+    var onLocationUpdate: ((CLLocationCoordinate2D?) -> Void)?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start(completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        onLocationUpdate = completion
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+        onLocationUpdate?(lastKnownLocation)
+        manager.stopUpdatingLocation() // Stop updating to save battery
     }
 }
 
