642f092e1735b46054294a63c6fa03ff54921f90
Night Ray <oskarkapusniak@gmail.com>
2024-12-06 11:58:41 +0100

    Help and support fix

--- MindfulnessApp/Settings/HelpAndSupportView.swift
+++ MindfulnessApp/Settings/HelpAndSupportView.swift
@@ -55,58 +55,58 @@ struct HelpAndSupportView: View {
                     if selectedSetting == 7{
                         
                         ScrollView{
-                            Text("Need Help?")
-                                .font(.system(size: 18, weight: .bold, design: .rounded))
-                                .foregroundStyle(.white)
-                                .padding(.top, 20)
-                            Text("If you encounter any issues or need assistance, please don't hesitate to contact us. We're here to help! Reach out to our support team, and we'll get back to you as soon as possible to resolve any concerns.")
-                                .font(.system(size: 16, weight: .medium, design: .rounded))
-                                .foregroundStyle(.white)
-                                .multilineTextAlignment(.center)
-                                .padding(.horizontal, 50)
-                                .padding(.top, 5)
-                            HStack{
-                                Image(systemName: "envelope.fill")
-                                    .font(.system(size: 16))
+                            VStack(spacing: 10){
+                                Text("Need Help?")
+                                    .font(.system(size: 18, weight: .bold, design: .rounded))
                                     .foregroundStyle(.white)
-                                    .multilineTextAlignment(.leading)
-                                    .padding(.leading, 50)
-                                    .padding(.top, 5)
-                                Text("mindflow@gmail.com")
+                                   
+                                Text("If you encounter any issues or need assistance, please don't hesitate to contact us. We're here to help! Reach out to our support team, and we'll get back to you as soon as possible to resolve any concerns.")
                                     .font(.system(size: 16, weight: .medium, design: .rounded))
                                     .foregroundStyle(.white)
-                                    .padding(.trailing, 50)
-                                    .padding(.top, 5)
-                            }
-                            HStack{
-                                Image(systemName: "phone.fill")
-                                    .font(.system(size: 16))
+                                    .multilineTextAlignment(.center)
+                                    .padding(.bottom)
+                                    
+                                HStack{
+                                    Image(systemName: "envelope.fill")
+                                        .font(.system(size: 16))
+                                        .foregroundStyle(.white)
+                                        .multilineTextAlignment(.leading)
+                                        
+                                    Text("mindflow@gmail.com")
+                                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                                        .foregroundStyle(.white)
+                                        
+                                }
+                                HStack{
+                                    Image(systemName: "phone.fill")
+                                        .font(.system(size: 16))
+                                        .foregroundStyle(.white)
+                                        .multilineTextAlignment(.leading)
+                                       
+                                    Text("+48 12 123 45 67")
+                                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                                        .foregroundStyle(.white)
+                                       
+                                }
+                                
+                                Text("Thank you for using our app!")
+                                    .font(.system(size: 18, weight: .bold, design: .rounded))
                                     .foregroundStyle(.white)
-                                    .multilineTextAlignment(.leading)
-                                    .padding(.leading, 50)
-                                    .padding(.top, 5)
-                                Text("+48 12 123 45 67")
-                                    .font(.system(size: 16, weight: .medium, design: .rounded))
-                                    .foregroundStyle(.white)
-                                    .padding(.trailing, 50)
-                                    .padding(.top, 5)
+                                    .padding(.top)
+                                    
                             }
-
-                            Text("Thank you for using our app!")
-                                .font(.system(size: 18, weight: .bold, design: .rounded))
-                                .foregroundStyle(.white)
-                                .padding(.top, 10)
+                            .frame(width:250)
                             
                         }
                         .scrollBounceBehavior(.basedOnSize)
                         .scrollIndicators(.hidden)
-                        .padding(.bottom, 5)
+                        .padding(.vertical, 10)
                     }
                     
                 }
                 
             }
-            .frame(width: 345, height: selectedSetting == 7 ? 375 : 50)
+            .frame(width: 345, height: selectedSetting == 7 ? 370 : 50)
             .background{
                 Rectangle()
                     .settingsViewColor()
