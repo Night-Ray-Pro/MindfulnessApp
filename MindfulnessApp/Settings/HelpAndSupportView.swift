70cb730fc186dcae83497110ee33a5c1cd59abc1
JacobKK10 <jakubkalina10@gmail.com>
2024-12-06 12:04:44 +0100

    HelpAndSupport - organized

--- MindfulnessApp/Settings/HelpAndSupportView.swift
+++ MindfulnessApp/Settings/HelpAndSupportView.swift
@@ -58,44 +58,38 @@ struct HelpAndSupportView: View {
                             VStack(spacing: 10){
                                 Text("Need Help?")
                                     .font(.system(size: 18, weight: .bold, design: .rounded))
-                                    .foregroundStyle(.white)
                                    
                                 Text("If you encounter any issues or need assistance, please don't hesitate to contact us. We're here to help! Reach out to our support team, and we'll get back to you as soon as possible to resolve any concerns.")
                                     .font(.system(size: 16, weight: .medium, design: .rounded))
-                                    .foregroundStyle(.white)
                                     .multilineTextAlignment(.center)
                                     .padding(.bottom)
                                     
                                 HStack{
                                     Image(systemName: "envelope.fill")
                                         .font(.system(size: 16))
-                                        .foregroundStyle(.white)
                                         .multilineTextAlignment(.leading)
                                         
                                     Text("mindflow@gmail.com")
                                         .font(.system(size: 16, weight: .medium, design: .rounded))
-                                        .foregroundStyle(.white)
                                         
                                 }
                                 HStack{
                                     Image(systemName: "phone.fill")
                                         .font(.system(size: 16))
-                                        .foregroundStyle(.white)
                                         .multilineTextAlignment(.leading)
                                        
                                     Text("+48 12 123 45 67")
                                         .font(.system(size: 16, weight: .medium, design: .rounded))
-                                        .foregroundStyle(.white)
                                        
                                 }
                                 
                                 Text("Thank you for using our app!")
                                     .font(.system(size: 18, weight: .bold, design: .rounded))
-                                    .foregroundStyle(.white)
                                     .padding(.top)
                                     
                             }
                             .frame(width:250)
+                            .foregroundStyle(.white)
                             
                         }
                         .scrollBounceBehavior(.basedOnSize)
